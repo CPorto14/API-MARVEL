804e2db371ae6cdd771f0a633c9bc85363ae11a3
CPorto14 <zprex14@gmail.com>
2023-04-02 17:57:07 -0500

    visualizacion de Hulk

--- marvel_app/lib/main.dart
+++ marvel_app/lib/main.dart
@@ -1,115 +1,95 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  Map<String, dynamic> _characterData = {};
+  bool _isLoading = true;
 
-  // This widget is the root of your application.
+  @override
+  void initState() {
+    super.initState();
+    _fetchCharacterData();
+  }
+
+  Future<void> _fetchCharacterData() async {
+    // Set up API request URL
+    final baseUrl = 'https://gateway.marvel.com/v1/public/characters';
+    final apiKey = '3dc609c85447e4e09621817160184c7b';
+    final privateKey = '1a5237e19c9bbbb71108148c8b84e7697125e8ef';
+    final ts = DateTime.now().millisecondsSinceEpoch.toString();
+    final preHash = ts + privateKey + apiKey;
+    final hash = md5.convert(utf8.encode(preHash)).toString();
+
+    final url =
+        '$baseUrl?name=Hulk&ts=$ts&apikey=$apiKey&hash=$hash&orderBy=name';
+
+    // Make API request
+    final response = await http.get(Uri.parse(url));
+    final responseData = json.decode(response.body);
+    final results = responseData['data']['results'];
+
+    if (results.isNotEmpty) {
+      setState(() {
+        _characterData = results[0];
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Personaje elegijo Hulk'),
         ),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Image.network(
+                      '${_characterData['thumbnail']['path']}.${_characterData['thumbnail']['extension']}',
+                      height: 200,
+                      width: 200,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      _characterData['name'],
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      _characterData['description'],
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    Text('Comics: ${_characterData['comics']['available']}'),
+                    SizedBox(height: 10),
+                    Text('Series: ${_characterData['series']['available']}'),
+                    SizedBox(height: 10),
+                    Text('Stories: ${_characterData['stories']['available']}'),
+                    SizedBox(height: 10),
+                    Text('Events: ${_characterData['events']['available']}'),
+                    SizedBox(height: 20),
+                    Text(' 3 primeras series:'),
+                    SizedBox(height: 10),
+                    for (var i = 0; i < 3; i++)
+                      Text('- ${_characterData['series']['items'][i]['name']}'),
+                  ],
+                ),
+              ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
